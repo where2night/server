abbb44659810f66468532b8fc3d000e47a94920c
Ismaibz <ismael.requena.andreu@gmail.com>
2014-01-20 21:46:47 +0100

    Editar Perfil Fiestero
    Tanto para enviar datos como para recibirlos se realiza con peticiones
    POST.
    Se cambiará en un futuro próximo para hacerlo por id

--- editprofile.php
+++ editprofile.php
@@ -10,37 +10,39 @@
 		$method = $_SERVER['REQUEST_METHOD'];
 		switch ($method) {
 		case 'POST':
-			if (isset($_POST["idProfile"])){
-			$idProfile = $_POST["idProfile"];
-			$picture = $_POST["picture"]; 
-			$name = $_POST["name"]; 
-			$surnames = $_POST["surnames"]; 
-			$birthdate = $_POST["birthdate"]; 
-			$birthdate=_formato_fechasFB($birthdate);
-			
-			$gender = $_POST["gender"]; 
-			$genderbool=false;
-			if($gender == 'male')$genderbool= true;
-			
-			$music = $_POST["music"]; 
-			$civil_state = $_POST["civil_state"]; 
-			$city = $_POST['city'];
-			$drink = $_POST["drink"]; 
-			$about = $_POST["about"]; 
-			
-			_db_connect();
-			$query = "call setPartierData('{$idProfile}','{$picture}','{$name}','{$surnames}','{$birthdate}','{$genderbool}','{$music}','{$civil_state}','{$city}','{$drink}','{$about}')";
-			_db_query($query);
-			
-			_db_close();
-			$sql = "call getPartierData(\'isma_ibz2@hotmail.com\')";
-			
+			if (isset($_POST["idProfile"])){			
+				$idProfile = $_POST["idProfile"];
+				$picture = $_POST["picture"]; 
+				$name = $_POST["name"]; 
+				$surnames = $_POST["surnames"]; 
+				$birthdate = $_POST["birthdate"]; 
+				$birthdate=_formato_fechasFB($birthdate);			
+				$gender = $_POST["gender"]; 
+				$genderbool=false;
+				if($gender == 'male')$genderbool= true;			
+				$music = $_POST["music"]; 
+				$civil_state = $_POST["civil_state"]; 
+				$city = $_POST['city'];
+				$drink = $_POST["drink"]; 
+				$about = $_POST["about"]; 
+				_setPartierData($idProfile,$name,$surnames,$birthdate,$genderbool,$music,$civil_state,$city,$drink,$about);
+				
+			}
+			if (isset($_POST["email"])){	
+					$email = $_POST["email"];
+					$data = _getPartierData($email);
+					echo json_encode($data);
+				
 			}			
 			break;	
 		case 'GET':
+		/*	**** Futura manera de pedir los datos ****
 		
-			
-			
+			$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+			$idPartier = $request[0];
+			$data = _getPartierData($idPartier)
+			echo json_encode($data);
+		*/
 			break;	
 		  default:
 		//	rest_error($request);  
