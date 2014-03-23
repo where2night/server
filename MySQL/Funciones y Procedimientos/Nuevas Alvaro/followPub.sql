18f996cd481ba0f662e7a735812c0783bff5a8c3
alvaropascualcrespo <alvaropascualcrespo@gmail.com>
2014-03-23 11:54:10 +0100

    More followers functions

---
+++ MySQL/Funciones y Procedimientos/Nuevas Alvaro/followPub.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE PROCEDURE `followPub`(idProfilePub INT, idProfileUser INT)
+BEGIN
+		
+		
+		SET @idU = (SELECT p.idPartier
+					FROM `Partier` p
+					WHERE p.idProfile = idProfileUser);
+
+		SET @idP = (SELECT p.idPub
+					FROM `Pub` pu
+					WHERE pu.idProfile = idProfilePub);
+
+		INSERT INTO `PartierFollowsPub` VALUES (@idU,@idP);
+
+		
+  END$$
