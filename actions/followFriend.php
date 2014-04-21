224d8356e1e7e7ce620b688b9eebb24f56e7a7b7
Luis <lecubero21@gmail.com>
2014-04-21 18:17:29 +0200

    Fix bugs followfriends

--- actions/followFriend.php
+++ actions/followFriend.php
@@ -4,35 +4,46 @@
 	require_once ("../utils.inc");
 	
 	$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
-	$idProfile = $request[0];
-	$token = $request[1];
-	$idUser = $request[2];
+
 	
-	$tokenT= _tokenOK($idProfile,$token);
 	
-	if (isset($_SERVER['REQUEST_METHOD']) && $tokenT == 1){
+	
+	if (isset($_SERVER['REQUEST_METHOD']) ){
 		$method = $_SERVER['REQUEST_METHOD'];
 		switch ($method) {
 			case 'POST':
-				if ($idProfile != "" && $idUser != ""){
-					if ($type == 0){ //Partier
-						$idProfile = $_POST["idProfile"];
-						$idUser = $_POST["idPartier"];
-						$mode = _getModeFollow($idProfile,$idUser);
+				if (($_POST["idPartier1"] != "")&& ($_POST["idPartier2"] != "")){
+				$idPartier1 = $_POST["idPartier1"];
+				$idPartier2 = $_POST["idPartier2"];
+				$idP1= _getIdPartier($idPartier1);
+				$idP2= _getIdPartier($idPartier2);
+			
+				$type1 = _getTypeProfile($idPartier1);
+				$type2 = _getTypeProfile($idPartier2);
+				
+
+					if ($type1 == 0 && $type2 == 0){ //Partier
+
+						$mode = _getModeFollow($idP1,$idP2);
 						if($mode == 0){
-							$idP1= _getIdPartier($idProfile);
-							$idP2= _getIdPartier($idUser);
+							
 							$mode=1;
 							$aux = _setModeFollowPartier($idP1,$idP2,$mode);
 							$aux['follow']=true;
-						}else{
+						}
+						else{
 							$aux["follow"]=false;
 						}
 
 					}else{
+					
 						$aux['follow'] = false;
 					}
+					echo json_encode($aux);
 				}
+			
+				
+
 				break;
 			case 'GET':
 				if ($idProfile != "" && $idUser != ""){
