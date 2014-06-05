14862b63fd10e34bd8c5aca50c64cf8788996249
ADRIAN D. <adriandiazjimenez@gmail.com>
2014-06-05 16:36:19 +0200

    fixed bug

--- read/messagesSorted.php
+++ read/messagesSorted.php
@@ -13,36 +13,42 @@
 		$method = $_SERVER['REQUEST_METHOD'];
 		switch ($method) {
 			case 'GET':
-				if ($idProfile != ""){
-					$aux1= _getPartiersMessages($idProfile);
-					$aux2= _checkMessagesUnread($idProfile);
-
-					if($aux2!=null){
-					$aux = array_merge($aux2,$aux1);
-					  for ($i=0; $i < sizeof($aux2) ; $i++){
-                                          array_pop($aux);
-                                           }
-                                        }
-                                        else {$aux=$aux1; }
-
-				}
 				
-				for ($i=0; $i < sizeof($aux) ; $i++) {
-                         	$idP = $aux[$i]['idProfile'];
-							$data[$i] = _getMessagesFriend2($idProfile,$idP);
-							$aux3=_getPartierData($idP);
-
-                            $num = sizeof($data[$i]);             
-	                        $data[$i]['idProfile']=$idP;  
-	                        $data[$i]['num']=$num; 
-	                        $data[$i]['name']=$aux3['name'];   
-	                        $data[$i]['surnames']=$aux3['surnames'];  
-	                        $data[$i]['picture']= $aux3['picture'];                                             
-								
-						} 
-
-
-				echo json_encode($data);
+			if ($idProfile != ""){
+					
+					
+					$aux=_getAllMessages($idProfile);
+					$aux1= _howManyMessages($idProfile);
+					$ini=0;
+					$num=$aux1['rows'];
+					for($i=0 ; $i<$num ; $i++){
+						$numMessage=$aux1[$i]['NUM'];
+						$idP= $aux1[$i]['idProfile'] ;
+						
+						$name= $aux1[$i]['name'];
+						$surnames= $aux1[$i]['surnames'];
+						$picture= $aux1[$i]['picture'];
+						$o=0;
+								for($s=$ini ; $s<$ini+$numMessage ; $s++){
+									
+									$res[$i][$o]=$aux[$s];
+									$o++;
+								}
+							
+						$ini=$ini+$numMessage;
+						$res[$i]['idProfile']= $idP ;
+						$res[$i]['num']=$numMessage;
+						$res[$i]['name']= $name;
+						$res[$i]['surnames']= $surnames;
+						$res[$i]['picture']= $picture;
+						
+						
+					}
+					}
+					echo json_encode($res);
+					
+					
+					
 			break;	
 
 		  default: 
